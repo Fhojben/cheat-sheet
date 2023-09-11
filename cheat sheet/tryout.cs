ebfede49310107cb963eb26ad9b25b6ea4350bbe
Fhojben <Albertvirum@gmail.com>
2023-09-11 11:08:05 +0200

    tryout

---
+++ cheat sheet/tryout.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web;
+
+namespace cheat_sheet
+{
+    public class tryout
+    {
+        private string brand;
+        private string coloer;
+        private int cost;
+        private int NOseats;
+        private int NOwheels;
+        private double topSpeed;
+    }
+}
